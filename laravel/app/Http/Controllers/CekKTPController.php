e1f47e32172a25657785fa9098e1748ace2030e1
Bertho Erizal <bertoerizal21@gmail.com>
2020-12-17 08:31:55 +0700

    hapus yang tidak penting

--- laravel/app/Http/Controllers/CekKTPController.php
+++ laravel/app/Http/Controllers/CekKTPController.php
@@ -37,26 +37,4 @@ class CekKTPController extends Controller
         $data = json_decode($response->getBody(), true);
         return view('cekktp.index', ['title' => $title, 'data' => $data]);
     }
-
-    public function perrecobaan(Request $request)
-    {
-        $client = new Client();
-
-        $api_key = 'c53c604b69f29678823081c30670eafe18bc9f7f8b6d17a1c89a9cc6a4a85d92';
-        $response = $client->request('POST', 'https://api.binderbyte.com/v1/validation/ktp?api_key=' . $api_key, [
-            'form_params' => [
-                'nik' => $request->nik,
-                'name' => $request->name,
-                'birthplace' => $request->birthplace,
-                'birthdate' => $request->birthdate
-            ]
-        ]);
-
-        return response()->json($response);
-
-        //jika ingin ditampilkan ke view pada halaman yang berbea:        
-        // $data = json_decode($response->getBody(), true);
-        // return view('dashboard', ['data' => $data]);
-        //kemudian pada file view panggil data dengan cara {{$data['nik']}}
-    }
 }
